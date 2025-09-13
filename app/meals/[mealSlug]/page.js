66bb999821517b97b9deee70f6769bf6afb58e37
Muhammad Arslan <arslan05342@gmail.com>
2025-09-13 18:25:52 +0500

    fina touch to application revalidation and metadata

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -4,6 +4,20 @@ import { notFound } from 'next/navigation'
 import classes from './page.module.css'
 import { getMeal } from '@/lib/meals'
 
+export async function generateMetaData({ params }){
+  const meal = getMeal(params.mealSlug)
+
+  if(!meal){
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.description
+  };
+}
+
+
 export default function mealDetailsPage({ params }){
   const meal = getMeal(params.mealSlug)
 
--- components/meals/share-meal-button.js
+++ components/meals/share-meal-button.js
@@ -1,7 +1,7 @@
 import { useFormStatus } from "react-dom"
 
 export default function BUtton(){
-  const {pending} = useFormStatus() // it ony work if it is in the form
+  const {pending} = useFormStatus() // it only work if it is in the form
   return (
     <button type="submit">{pending ? 'Sharing meal.....' : 'Share Meal'}</button>
   )
